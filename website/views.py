d2d293f403de49ef97aacf36d426e079e4882af8
Hassan Ahmed Fath <hassan.ahmed.dev98@hotmail.com>
2025-10-31 19:24:11 +0200

    Update views.py

--- website/views.py
+++ website/views.py
@@ -78,7 +78,7 @@ def register(request):
 
 def customer_record(request, pk):
 	if request.user.is_authenticated:
-		# Look Up Records
+		
 		customer_record = Record.objects.get(id=pk)
 		return render(request, 'record.html', {'customer_record':customer_record})
 	else:
